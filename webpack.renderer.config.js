cd475ff3a832a1246bec15b318046ee4d6ed51f8
Stephen Taylor <stephen.taylor.dev@gmail.com>
2021-07-26 10:39:31 -0500

    added ttf loader (just in case)

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,9 +1,15 @@
 const rules = require('./webpack.rules');
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  },
+  {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
+  }
+);
 
 module.exports = {
   // Put your normal webpack config below here
